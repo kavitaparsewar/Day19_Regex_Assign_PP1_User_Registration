3b88aec23a504320e94313af69594e68fe97f0d1
kavitaparsewar <parsewarkavita@gmail.com>
2021-10-04 17:02:22 +0530

    [kavita] added UC3_UC9 i.e validation for gmail

---
+++ UC3_UC9_valid_email.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PP1_User_Registration
+{
+    class UC3_UC9_valid_email
+    {
+        public static string validation = "^[a-zA-Z]{3,}[.][a-zA-Z]{3,}[@][a-zA-Z]{2}[.][a-zA-Z]{2}$";
+        public bool validemail(string name)
+        {
+            return Regex.IsMatch(name, validation);
+        }
+    
+    }
+}
