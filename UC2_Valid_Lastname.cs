ab0eaf132554a5d7c2bee518fb852038bde21f01
kavitaparsewar <parsewarkavita@gmail.com>
2021-10-04 17:00:48 +0530

    [kavita] added UC2 i.e validation for last name

---
+++ UC2_Valid_Lastname.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PP1_User_Registration
+{
+    class UC2_Valid_Lastname
+    {
+        public static string validationlastname = "^[A-Z]{3,}$";
+        
+        public bool validlname(string name)
+        {
+            return Regex.IsMatch(name, validationlastname);
+        }
+    }
+}
