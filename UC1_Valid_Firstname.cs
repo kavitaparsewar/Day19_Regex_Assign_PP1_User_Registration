3641d9d50d938c5c26814ce60119462899400b0d
kavitaparsewar <parsewarkavita@gmail.com>
2021-10-04 16:59:44 +0530

    [kavita] added UC1 i.e validation for first name

---
+++ UC1_Valid_Firstname.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PP1_User_Registration
+{
+    class UC1_Valid_Firstname
+    {
+        public static string validationfirstname = "^[A-Z]{3,}$";
+        public bool validfname(string name)
+        {
+            return Regex.IsMatch(name, validationfirstname);
+        }
+    
+    }
+ }
