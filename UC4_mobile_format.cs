14b7736b7026f82b9bc6a38477f2226c777ae374
kavitaparsewar <parsewarkavita@gmail.com>
2021-10-04 17:03:52 +0530

    [kavita] added UC4 i.e validation for mobile number

---
+++ UC4_mobile_format.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PP1_User_Registration
+{
+    class UC4_mobile_format
+    {
+        public static string validation = "^[91]+[ ]+[0-9]{10}$";
+        public bool validmobile(string name)
+        {
+            return Regex.IsMatch(name, validation);
+        }
+    }
+}
