949ef29fed1edf77b16f9164511f214daedc8e63
kavitaparsewar <parsewarkavita@gmail.com>
2021-10-05 22:37:47 +0530

    [kavita] added UC5 Password

---
+++ UC5_6_7_8.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PP1_User_Registration
+{
+    class UC5_6_7_8
+    {    
+        public static string validationmob = "^((?=.*[a-zA-Z0-9])(?=.*[#%=]).{8,})";       
+       //UC5,UC6,UC7,UC8
+
+        public bool validation(string name)
+        {
+            return Regex.IsMatch(name, validationmob);
+        }
+    }
+}
