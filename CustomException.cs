098997cc655aebb75546ef9745640f854682cc75
kavitaparsewar <parsewarkavita@gmail.com>
2021-11-07 23:53:27 +0530

    [kavita] added custom exception in regex

---
+++ CustomException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PP1_User_Registration
+{
+    public class CustomException : Exception
+    {
+        public enum ExceptionsType
+        {
+            INVALID_MESSAGE
+        }
+        public ExceptionsType type;
+        public CustomException(ExceptionsType Type, string message) : base(message)
+        {
+            this.type = Type;
+
+        }
+    }
+}
