6ef5783d0a85a919c6b003f2a9164137fb49bd41
kavitaparsewar <parsewarkavita@gmail.com>
2021-11-07 23:55:09 +0530

    added Uc2

--- UC2_Valid_Lastname.cs
+++ UC2_Valid_Lastname.cs
@@ -7,10 +7,14 @@ namespace PP1_User_Registration
 {
     class UC2_Valid_Lastname
     {
-        public static string validationlastname = "^[A-Z]{3,}$";
-        
+        public static string validationlastname = "^[A-Z]{1}[a-z]{2,}$";
+
         public bool validlname(string name)
         {
+            if (name == null)
+            {
+                throw new CustomException(CustomException.ExceptionsType.INVALID_MESSAGE, "Invalid FirstName");
+            }
             return Regex.IsMatch(name, validationlastname);
         }
     }
