33ed394ce2e3c368c265cfcfab563e7ef2a8cf66
kavitaparsewar <parsewarkavita@gmail.com>
2021-11-07 23:30:05 +0530

    added Uc1

--- UC1_Valid_Firstname.cs
+++ UC1_Valid_Firstname.cs
@@ -7,9 +7,14 @@ namespace PP1_User_Registration
 {
     class UC1_Valid_Firstname
     {
-        public static string validationfirstname = "^[A-Z]{3,}$";
+        public static string validationfirstname = "^[A-Z]{1}[a-z]{2,}$";
+       
         public bool validfname(string name)
         {
+            if (name == null)
+            {
+                throw new CustomException(CustomException.ExceptionsType.INVALID_MESSAGE, "Invalid FirstName");
+            }
             return Regex.IsMatch(name, validationfirstname);
         }
     
