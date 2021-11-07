cf80590dc92fa49b9a87236f58c428070ceb8c59
kavitaparsewar <parsewarkavita@gmail.com>
2021-11-08 00:08:01 +0530

    added Uc4

--- UC4_mobile_format.cs
+++ UC4_mobile_format.cs
@@ -8,8 +8,13 @@ namespace PP1_User_Registration
     class UC4_mobile_format
     {
         public static string validation = "^[91]+[ ]+[0-9]{10}$";
+        //"^[0-9]{2}[ ][0-9]{10}$";
         public bool validmobile(string name)
         {
+            if (name == null)
+            {
+                throw new CustomException(CustomException.ExceptionsType.INVALID_MESSAGE, "Invalid FirstName");
+            }
             return Regex.IsMatch(name, validation);
         }
     }
