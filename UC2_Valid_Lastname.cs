cc986513772bdaaabb549cc7ed0e223e4a792e5c
kavitaparsewar <parsewarkavita@gmail.com>
2021-11-08 19:58:45 +0530

    [kavita] modified UC2

--- UC2_Valid_Lastname.cs
+++ UC2_Valid_Lastname.cs
@@ -1,21 +1,21 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
 
-//namespace PP1_User_Registration
-//{
-//    class UC2_Valid_Lastname
-//    {
-//        public static string validationlastname = "^[A-Z]{1}[a-z]{2,}$";
+namespace PP1_User_Registration
+{
+    class UC2_Valid_Lastname
+    {
+        public static string validationlastname = "^[A-Z]{1}[a-z]{2,}$";
 
-//        public bool validlname(string name)
-//        {
-//            if (name == null)
-//            {
-//                throw new CustomException(CustomException.ExceptionsType.INVALID_MESSAGE, "Invalid FirstName");
-//            }
-//            return Regex.IsMatch(name, validationlastname);
-//        }
-//    }
-//}
+        public bool validlname(string name)
+        {
+            if (name == null)
+            {
+                throw new CustomException(CustomException.ExceptionsType.INVALID_MESSAGE, "Invalid FirstName");
+            }
+            return Regex.IsMatch(name, validationlastname);
+        }
+    }
+}
